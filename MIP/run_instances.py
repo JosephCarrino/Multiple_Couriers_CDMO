f5f40d407880b36bed99dd83a978995a5e657450
JosephCarrino <josephcarrinospam@gmail.com>
2023-07-03 17:32:00 +0200

    Fixed run_instances for MIP and SMT

--- MIP/run_instances.py
+++ MIP/run_instances.py
@@ -1,11 +1,11 @@
-from SMTordinecorretto import solve_one
+from MIP import solve_one
 from converter import get_file
 import multiprocessing
 import json
 import matplotlib.pyplot as plt
 import numpy as np
 
-N_INST = 9
+N_INST = 1
 FONTSIZE = 14
 
 def main():
@@ -17,18 +17,16 @@ def main():
         time_passed = multiprocessing.Queue()
         iter = multiprocessing.Queue()
 
-        p = multiprocessing.Process(target=solve_one, args=(instances, i, min_dist, sol, time_passed, iter))
+        p = multiprocessing.Process(target=solve_one, args=(instances, i, min_dist, sol, time_passed))
         p.start()
         p.join(300)
         if p.is_alive():
             print("TIMEOUT")
             p.terminate()
             p.join()
-
         to_print["sol"].append(sol.get() if not sol.empty() else "Unsat")
         to_print["min_dist"].append(min_dist.get() if not min_dist.empty() else 0)
         to_print["time_passed"].append(time_passed.get() if not time_passed.empty() else 300)
-        to_print["iter"].append(iter.get() if not iter.empty() else 0)
         print(to_print)
     with open("SAT_data.json", "w+") as f:
         json.dump(to_print, f)
--- SAT/SAT.py
+++ SAT/SAT.py
@@ -4,56 +4,75 @@ from itertools import combinations
 from converter import get_file
 from time import time
 
+
 def at_least_one(bool_vars):
     return Or(bool_vars)
+
 
 def at_most_one(bool_vars):
     return [Not(And(pair[0], pair[1])) for pair in combinations(bool_vars, 2)]
 
+
 def exactly_one(bool_vars):
     return at_most_one(bool_vars) + [at_least_one(bool_vars)]
+
 
 def at_most_k_seq(x, k, name):
     clauses = []
     n = len(x)
-    s =[[Bool(f"s_{i}_{j}_{name}") for j in range(k)] for i in range(n - 1)]
+    s = [[Bool(f"s_{i}_{j}_{name}") for j in range(k)] for i in range(n - 1)]
     clauses.append(Or(Not(x[0]), s[0][0]))
     clauses += [Not(s[0][j]) for j in range(1, k)]
-    clauses.append(Or( Not(x[n - 1]), Not(s[ n-2 ][ k-1 ])))
+    clauses.append(Or(Not(x[n - 1]), Not(s[n - 2][k - 1])))
     for i in range(1, n - 1):
-      clauses.append(Or(Not(x[i]), s[i][0]))
-      clauses.append(Or(Not(s[i-1][0]), s[i][0]))
-      for j in range(1, k):
-        clauses.append(Or(Not(s[i-1][j]), s[i][j]))
-        clauses.append(Or(Not(x[i]), Not(s[i-1][j-1]), s[i][j]))
-      clauses.append(Or(Not(x[i]), Not(s[ i-1 ][ k-1 ])))
-    return And(clauses) 
+        clauses.append(Or(Not(x[i]), s[i][0]))
+        clauses.append(Or(Not(s[i - 1][0]), s[i][0]))
+        for j in range(1, k):
+            clauses.append(Or(Not(s[i - 1][j]), s[i][j]))
+            clauses.append(Or(Not(x[i]), Not(s[i - 1][j - 1]), s[i][j]))
+        clauses.append(Or(Not(x[i]), Not(s[i - 1][k - 1])))
+    return And(clauses)
+
 
 MAXITER = 500
 
-def multiple_couriers(m, n, D, l, s): #m is the number of couriers, n is the number of packages, D is the distance matrix, l is the load of each courier, s is the size of each package
 
+def multiple_couriers(
+    m, n, D, l, s
+):  # m is the number of couriers, n is the number of packages, D is the distance matrix, l is the load of each courier, s is the size of each package
     # i is the courier
     # j is the step
     # k is the one-hot encoding of the package
-    c = [[[Bool(f"c_{i}_{j}_{k}") for k in range(n+1)] for j in range(n+2)] for i in range(m)]
+    c = [
+        [[Bool(f"c_{i}_{j}_{k}") for k in range(n + 1)] for j in range(n + 2)]
+        for i in range(m)
+    ]
 
     # i is the courier
     # j is the package
     # k is the bitwise encoding of the package
-    w = [[[Bool(f"w_{i}_{j}_{k}") for k in range(s[j])] for j in range(n)] for i in range(m)]
+    w = [
+        [[Bool(f"w_{i}_{j}_{k}") for k in range(s[j])] for j in range(n)]
+        for i in range(m)
+    ]
 
     # i is the courier
     # start is a position of the route
     # end is the next position of the route
-    d = [[[Bool(f"d_{i}_{start}_{end}") for end in range(n+1)] for start in range(n+1)] for i in range(m)]
+    d = [
+        [
+            [Bool(f"d_{i}_{start}_{end}") for end in range(n + 1)]
+            for start in range(n + 1)
+        ]
+        for i in range(m)
+    ]
 
     # Defining the solver instance
     solver = Solver()
 
     # Binding the two matrixes
     for i in range(m):
-        for j in range(1, n+1):
+        for j in range(1, n + 1):
             for package in range(n):
                 for k in range(s[package]):
                     solver.add(Implies(c[i][j][package], w[i][package][k]))
@@ -62,20 +81,22 @@ def multiple_couriers(m, n, D, l, s): #m is the number of couriers, n is the num
     # Binding the routing
     for i in range(m):
         for j in range(n):
-            for start in range(n+1):
-                for end in range(n+1):
-                    solver.add(Implies(And(c[i][j][start], c[i][j+1][end]), d[i][start][end]))
+            for start in range(n + 1):
+                for end in range(n + 1):
+                    solver.add(
+                        Implies(And(c[i][j][start], c[i][j + 1][end]), d[i][start][end])
+                    )
 
     # At each time, the courier carries exactly one package or he is at base
     for i in range(m):
-        for j in range(n+2):
+        for j in range(n + 2):
             solver.add(exactly_one(c[i][j][:]))
 
     # Each package is carried only once
     for package in range(n):
         vars = []
         for i in range(m):
-            for j in range(1, n+1):
+            for j in range(1, n + 1):
                 vars.append(c[i][j][package])
         solver.add(exactly_one(vars))
 
@@ -85,23 +106,21 @@ def multiple_couriers(m, n, D, l, s): #m is the number of couriers, n is the num
         vars = []
         for j in range(n):
             for k in range(s[j]):
-                vars.append(w[i][j][k])     
+                vars.append(w[i][j][k])
         solver.add(at_most_k_seq(vars, l[i], f"weight_{i}"))
 
     # At start/end the courier must be at the base
     for i in range(m):
         solver.add(c[i][0][n])
-        solver.add(c[i][n+1][n])
-
+        solver.add(c[i][n + 1][n])
 
     ## Breaking Symmetry ##
 
     # Lexicographic order for each courier
     for i in range(m):
         for j in range(1, n):
-            for z in range(j+1, n+1):
+            for z in range(j + 1, n + 1):
                 solver.add(Implies(c[i][j][n], c[i][z][n]))
-
 
     min_dist = 9999999
     for i in range(len(D)):
@@ -112,15 +131,15 @@ def multiple_couriers(m, n, D, l, s): #m is the number of couriers, n is the num
     max_dist = 0
     for i in range(len(D)):
         max_dist += max(D[i])
-    
+
     for i in range(m):
         dists = []
-        for start in range(n+1):
-            for end in range(n+1):
+        for start in range(n + 1):
+            for end in range(n + 1):
                 for z in range(D[start][end]):
                     dists.append(d[i][start][end])
 
-    iter = 1   
+    iter = 1
     start_time = time()
     last_sol = None
     while iter < MAXITER:
@@ -142,33 +161,35 @@ def multiple_couriers(m, n, D, l, s): #m is the number of couriers, n is the num
                 return k, last_sol, f"{time() - start_time:.2f}", iter
             else:
                 return 0, "Unsat", f"{time() - start_time:.2f}", iter
-        iter+=1
+        iter += 1
         solver.pop()
     return k, last_sol, f"{time() - start_time:.2f}", iter
 
 
-def minimizer_binary(instance, solver=multiple_couriers, maxiter = MAXITER):
-  m = instance["m"]
-  n = instance["n"]
-  D = instance["D"]
-  l = instance["l"]
-  s = instance["s"]
+def minimizer_binary(instance, solver=multiple_couriers, maxiter=MAXITER):
+    m = instance["m"]
+    n = instance["n"]
+    D = instance["D"]
+    l = instance["l"]
+    s = instance["s"]
+    print(instance)
+    return solver(m, n, D, l, s)
 
-  return solver(m, n, D, l, s)
-  
 
-def solve_one(instances, idx, to_ret1 = None, to_ret2 = None, to_ret3 = None, to_ret4 = None):
-    sol, mindist, time_passed, iter =  minimizer_binary(instances[idx])
+def solve_one(instances, idx, to_ret1=None, to_ret2=None, to_ret3=None, to_ret4=None):
+    sol, mindist, time_passed, iter = minimizer_binary(instances[idx])
     if to_ret1 != None:
         to_ret1.put(sol)
         to_ret2.put(mindist)
         to_ret3.put(time_passed)
         to_ret4.put(iter)
     return sol, mindist, time_passed, iter
-  
+
+
 def main():
     instances = get_file()
     solve_one(instances, 1)
+
 
 if __name__ == "__main__":
     main()
